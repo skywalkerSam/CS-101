aa9dd592740a17bb79dc1ca387b28363079ce225
Starboy <samskywalker001@gmail.com>
2025-02-03 20:10:46 +0530

    initials

--- Introduction-with-Python3/Lec-1/lec1.py
+++ Introduction-with-Python3/Lec-1/lec1.py
@@ -1,12 +1,12 @@
 pi = 3.14159
-radius = 2.2
+radius = 3
 # area of circle equation <- this is a comment
 area = pi*(radius**2)
 print(area)
 
 # change values of radius <- another comment
 # use comments to help others understand what you are doing in code
-radius = radius + 1
+radius = radius + 6
 print(area)     # area doesn't change
 area = pi*(radius**2)
 print(area)
@@ -19,11 +19,11 @@ print(area)
 # do CTRL+1 again to uncomment them
 # try it on the next few lines below!
 
-#area = pi*(radius**2)
-#print(area)
-#radius = radius + 1
-#area = pi*(radius**2)
-#print(area)
+area = pi*(radius**2)
+print(area)
+radius = radius - 6
+area = pi*(radius**2)
+print(area)
 
 #############################
 #### AUTOCOMPLETE #######
